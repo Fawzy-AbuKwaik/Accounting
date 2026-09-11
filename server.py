2bb06f42f505501a3cec89dedcf2ed5d1b00f60b
Fawzy Abu Kwaik <1201151@student.birzeit.edu>
2026-09-11 19:59:41 +0300

    adding POST request to extract the message as JSON

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Request # FastAPI let me create my web server/app, 
 # Normally FastAPI returns JSON responses
 from fastapi.responses import PlainTextResponse # PlainTextResponse return a plain text response. We need it to the webhook verification
 
-app = FastAPI() # creat the FastAPI app intance. so I run: python -m uvicorn server:app --reload, where server is server.py and app = FastAPI()
-VERIFY_TOKEN = "my_verify_Token_123" # This is simply a variable containing a secret-ish verification string that I choose, when configuring the webhook with Meta, I'll give Meta the same value
+app = FastAPI() # creat the FastAPI app intance. so I run: "python -m uvicorn server:app --reload", where server is server.py and app = FastAPI()
+VERIFY_TOKEN = "my_verify_Token_55" # This is simply a variable containing a secret-ish verification string that I choose, when configuring the webhook with Meta, I'll give Meta the same value
 
 @app.get("/") # this line called "Decorator", If someone sends a GET request to /, execute the function directly below. "http//127.0.0.1:8000/"
 def home():
@@ -26,3 +26,8 @@ async def verify_webhook(request: Request):
         return PlainTextResponse(content = challenge, status_code = 200) # return a plain text response with the challenge value and a status code of 200 (OK). This tells Meta that the verification was successful and that I own the webhook endpoint.
     return PlainTextResponse(content = "Verification failed", status_code = 403) # If the mode is not "subscribe" or the token does not match my VERIFY_TOKEN, it means that the request is either not a verification request or is not coming from Meta. In this case, I return a plain text response with the content "Verification failed" and a status code of 403 (Forbidden). This tells Meta that the verification failed and that I do not own the webhook endpoint.
         
+@app.post("/webhook") # if someone sends a POST request to /webhook, execute the function directly below. This is the webhook endpoint that Meta will call when there is a new message or event for my WhatsApp Business account.
+async def receive_wehook(request: Request):
+    data = await request.json() # read the incoming request body as JSON and store it in the variable data. This is where I will receive the actual message or event data from Meta.
+    print(data) # print the received data to the console for debugging purposes. This allows me to see the structure and content of the incoming message or event data.
+    return {"status": "received"}
